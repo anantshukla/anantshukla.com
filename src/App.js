2a359c8e1c1ed35e6988c30beee497a783055935
Anant Shukla <accounts@anantshukla.com>
2024-02-28 19:25:41 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -22,27 +22,27 @@ function App() {
         <h2>Contact Information</h2>
         <div className="content">
           <div className="contact-item">
-            <span className="label">Phone:</span>
-            <span className="value">(438) 979-3841</span>
+            <span className="label">Phone: </span>
+            <span className="value">+1 (438) 979-3841</span>
           </div>
           <div className="contact-item">
-            <span className="label">Location:</span>
+            <span className="label">Location: </span>
             <span className="value">Toronto, ON</span>
           </div>
           <div className="contact-item">
-            <span className="label">Email:</span>
+            <span className="label">Email: </span>
             <span className="value"><a href="mailto:me@anantshukla.com">me@anantshukla.com</a></span>
           </div>
           <div className="contact-item">
-            <span className="label">LinkedIn:</span>
+            <span className="label">LinkedIn: </span>
             <span className="value"><a href="https://linkedin.com/in/theanantshukla">theanantshukla</a></span>
           </div>
           <div className="contact-item">
-            <span className="label">GitHub:</span>
+            <span className="label">GitHub: </span>
             <span className="value"><a href="https://github.com/anantshukla">anantshukla</a></span>
           </div>
           <div className="contact-item">
-            <span className="label">Website:</span>
+            <span className="label">Website: </span>
             <span className="value"><a href="https://anantshukla.com">anantshukla.com</a></span>
           </div>
         </div>
